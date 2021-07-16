aafb8a9b5490be28dcc23aa08b22a0fb89e9679d
Meltin25 <87095987+Meltin25@users.noreply.github.com>
2021-07-16 21:54:18 +0200

    Add files via upload

---
+++ testing.py
@@ -0,0 +1,29 @@
+import unittest
+
+
+def nextPermutations(myList):
+    pass
+
+
+class nextTest(unittest.TestCase):
+    # test the output of the correct value
+    def test_next(self):
+        # list to test
+        myList = [1, 2, 3]
+        # function call to be tested
+        result = nextPermutations(myList)
+        # assert
+        self.assertEqual(result, [1, 3, 2])
+        # test for the wrong output
+
+    def test_next_not_equal(self):
+        # list to test
+        myList = [1, 2, 3]
+        # function call to be tested
+        result = nextPermutations(myList)
+        # assert
+        self.assertNotEqual(result, [1, 2, 3])
+
+
+if __name__ == '__main__':
+    unittest.main()
