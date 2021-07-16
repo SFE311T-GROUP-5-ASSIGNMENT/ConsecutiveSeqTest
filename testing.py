c81f7848858414a573b895fa055a7d006988228f
Meltin25 <87095987+Meltin25@users.noreply.github.com>
2021-07-16 22:42:21 +0200

    Update testing.py

--- testing.py
+++ testing.py
@@ -1,16 +1,18 @@
 import unittest
-from longestConsecutiveSeq import longestConsecutiveSeq
+from longestConsecutiveSeq import longestConsecutive
 
-from long import longestConsecutive
+
 class longTest(unittest.TestCase):
     def test_long(self):
         myList = [0, 3, 7, 2, 5, 8, 4, 6, 0, 1]
         result = longestConsecutive(myList)
-        self.assertEqual(result, 9)    
+        self.assertEqual(result, 9)
+
     def test_long_not_equal(self):
         myList = [0, 3, 7, 2, 5, 8, 4, 6, 0, 1]
         result = longestConsecutive(myList)
         self.assertNotEqual(result, 8)
-        
+
+
 if __name__ == '__main__':
     unittest.main()
